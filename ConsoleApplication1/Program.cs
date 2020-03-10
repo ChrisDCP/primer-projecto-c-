631bc98136038934bbaed447f107a240cf8633a4
JORDYasa <alvarezjordi609@gamil.com>
2020-03-10 17:26:06 -0600

    primer projecto

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n, i;
+            Console.WriteLine("escriba el primer numero");
+            m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("escriba el segundo numero");
+            n = int.Parse(Console.ReadLine());
+
+            for (i = m; i <= n; i++)
+
+            {
+                if (i % 2 == 0)
+
+                    Console.WriteLine(i);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
